3825158d22873b0578fcd70cad478d4f9e486098
Alexandre Dantas <alexandredantas690@outlook.com>
2024-05-07 08:29:45 -0300

    Adição da pasta LeetCode e solução 3090 (LeetCode)

---
+++ LeetCode/3090.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int MaximumLengthSubstring(string str) {
+        SortedDictionary<char, int> myMap = new SortedDictionary<char, int>();
+        while (end < str.Length) {
+            if (myMap.ContainsKey(str[end])) { // contains the letter
+                if (myMap[str[end]] < 2) {
+                    myMap[str[end]]++;
+                    end++;
+                }
+                else {
+                    resp = (end - start) > resp ? (end - start) : resp;
+                    myMap[str[start]]--;
+                    start++;
+                }
+            }
+            else {
+                myMap.Add(str[end], 1);
+                end++;
+            }
+        }
+        resp = (end - start) > resp ? (end - start) : resp;
+        return resp;
+    }
+}
